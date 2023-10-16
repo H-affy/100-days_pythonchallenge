f7b6be6b0e801083bb5308bf5587568a9669acbb
H-affy <abdullateefadeluola@gmail.com>
2023-10-16 22:48:56 +0100

    Debugging and string manipulation

--- Day1-challenge/printing.py
+++ Day1-challenge/printing.py
@@ -1,3 +1,10 @@
+#Printing
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+#Debugging: String manipulation.
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
