8247f9f9f7534965c5650e00dd856904ca22a930
H-affy <abdullateefadeluola@gmail.com>
2023-10-19 13:15:32 +0100

    Input function

--- Day1-challenge/printing.py
+++ Day1-challenge/printing.py
@@ -8,3 +8,10 @@ print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+#Input function
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Write your code below this line 👇
+name = len(input("What's your name? "))
+print(name)
