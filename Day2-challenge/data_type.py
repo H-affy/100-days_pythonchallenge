f3ec16b9cdfcc04437c2f92326b1c48efc5c7c9f
H-affy <abdullateefadeluola@gmail.com>
2023-10-20 09:37:39 +0100

    Mathematical operation

--- Day2-challenge/data_type.py
+++ Day2-challenge/data_type.py
@@ -3,3 +3,16 @@ first_digit = int(two_digit_number[0])
 second_digit = int(two_digit_number[1])
 two_digit_number = (first_digit + second_digit)
 print(two_digit_number)
+
+#Mathematical operations
+#Create a BMI calculator using type conversion and mathematical operations
+height = input("Enter your height in meters:\n")
+weight = input("Enter your weight in kilograms:\n")
+#convert the height and weight using type conversion
+Height = float(height)
+Weight = int(weight)
+#using mathematical operation to calculate the bmi
+bmi = Weight/Height ** 2
+#using the round function to round to whole number
+BMI = round(bmi)
+print(BMI)
