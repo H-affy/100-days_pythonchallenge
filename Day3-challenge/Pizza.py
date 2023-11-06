a19688ee72e925d16ef81e68f5df783c06079131
H-affy <abdullateefadeluola@gmail.com>
2023-11-06 12:52:11 +0100

    Logical operators

--- Day3-challenge/Pizza.py
+++ Day3-challenge/Pizza.py
@@ -2,8 +2,7 @@ print("Thank you for choosing Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L\n") 
 add_pepperoni = input("Do you want pepperoni? Y or N\n") 
 extra_cheese = input("Do you want extra cheese? Y or N\n")
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
+
 bill = 0
 
 if size == "S":
