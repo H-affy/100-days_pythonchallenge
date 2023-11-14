99dbb7550059179c4dd24ee69ef4a679b75045c9
H-affy <abdullateefadeluola@gmail.com>
2023-11-14 01:38:09 +0100

    Rock paper Scissors

---
+++ Day4-challenge/rock.py
@@ -0,0 +1,32 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+import random
+
+player_choice = input("What do you choose? Type 0 for Rock, 1 for Paper, and 2 for Scissors\n")
+computer_choice = random.randint(0, 2)
+print(computer_choice)
