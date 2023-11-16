3c65b0a1891b805dfb77e6379e31e611203c89f3
H-affy <abdullateefadeluola@gmail.com>
2023-11-16 12:10:26 +0100

    Randimization and list

--- Day4-challenge/rock.py
+++ Day4-challenge/rock.py
@@ -26,7 +26,27 @@ scissors = '''
 '''
 
 import random
+game = [rock, paper, scissors]
 
-player_choice = input("What do you choose? Type 0 for Rock, 1 for Paper, and 2 for Scissors\n")
+
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, and 2 for Scissors\n"))
+print(game[player_choice])
+
 computer_choice = random.randint(0, 2)
-print(computer_choice)
+print(f"The computer choose: ")
+print(game[computer_choice])
+
+if player_choice == 0 and computer_choice == 2:
+    print("You win")
+elif player_choice == 2 and computer_choice == 1:
+    print("You win")
+elif player_choice == 2 and computer_choice == 0:
+    print("You lose")
+elif player_choice == 1 and computer_choice == 0:
+    print("You win")
+elif computer_choice == player_choice:
+    print("it a draw")
+elif computer_choice > player_choice:
+    print("You lose")
+else:
+    print("You entered an invalid number")
